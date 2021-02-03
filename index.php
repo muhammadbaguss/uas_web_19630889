0ceceeaade06df7fc10a3cd380d37821a0278f03
muhammadbaguss <76562149+muhammadbaguss@users.noreply.github.com>
2021-02-03 21:41:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="js/jquery-3.4.1.min.js"></script>
+</head>
+
+<body>
+  <div class="container">
+      <div class="row"></div>
+        <div class="center"></div>
+          <div class="class container col-md-12 bg-danger text-white text-center">
+            <h2>HALAMAN LOGIN</h2>
+          </div>
+               <form action="home.php" method="post">
+
+                  <div class="form-group">
+                  <h6>user</h6>
+                     <input type="text" name="username" class="form-control">
+                   </div>
+            
+                   <div class="form-group">
+                   <h6>password</h6>
+                    <input type="password" name="password" class="form-control">
+                    </div>
+                    <td></td>
+                    <td><input type="submit" name="login" value="Log In"></td>
+                    </tr>
+                    </table>
+                </form>
+        </div>
+      </div>
+    </div>
+</body>
+
+</html>
